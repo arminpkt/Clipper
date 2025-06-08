23ba19be91e8f583dbe953232bab90e9f4e7d073
arminpkt <64209484+arminpkt@users.noreply.github.com>
2025-06-08 04:15:37 +0200

    removed absolute file path in pluginpocessor

--- Clipper/PluginProcessor.h
+++ Clipper/PluginProcessor.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <juce_audio_processors/juce_audio_processors.h>
-#include "/Users/armin/Desktop/Uni/HKU/juceprojects/Clipper/Clipper/Clipper.h"
+#include "Clipper.h"
 
 //==============================================================================
 class AudioPluginAudioProcessor final : public juce::AudioProcessor
